8313d311bee206cb843ba47ca518bc7077a24e22
lfgtavora <lfgtavora@outlook.com>
2024-01-30 01:39:20 -0300

    update animations clicks in PortalItemView.kt

--- app/src/main/java/com/example/portals/MainActivity.kt
+++ app/src/main/java/com/example/portals/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.portals
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,6 +31,9 @@ import com.example.portals.core.Click
 import com.example.portals.core.MyCustomPortalObj
 import com.example.portals.core.MyCustomStepObj
 import com.example.portals.core.Portal
+import com.example.portals.ui.components.DefaultItemView
+import com.example.portals.ui.components.Image
+import com.example.portals.ui.components.Ring
 import com.example.portals.ui.theme.PortalsTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,17 +42,61 @@ class MainActivity : ComponentActivity() {
         setContent {
             PortalsTheme {
                 PortalList(
-                    items = mutableListOf(),
+                    items = mutableListOf(
+                        MyCustomPortalObj(
+                            title = "title 1",
+                            description = "description 1",
+                            image = "https://www.picsum.photos/128/128",
+                            steppers = mutableListOf(
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                            )
+                        ),
+                        MyCustomPortalObj(
+                            title = "title 2",
+                            description = "description 2",
+                            image = "https://www.picsum.photos/128/129",
+                            steppers = mutableListOf(
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                            )
+                        ),
+                        Portal(
+                            steppers = mutableListOf(
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                                MyCustomStepObj(),
+                            )
+                        )
+                    ),
                     renderView = {
                         when (it) {
-                            is MyCustomPortalObj -> CustomPortalItemView2(
-                                title = it.title,
-                                modifier = Modifier
-                                    .size(30.dp)
-                                    .clip(CircleShape)
-                                    .background(Color.Red),
+                            is MyCustomPortalObj -> DefaultItemView(
+                                image = Image(
+                                    source = it.image,
+                                    contentDescription = it.title
+                                ),
+                                ring = Ring(width = 3.dp, color = Color.Green),
+                                onClick = {
+                                    Toast.makeText(
+                                        this,
+                                        "click",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                },
+                                onLongClick = {
+                                    Toast.makeText(
+                                        this,
+                                        "long click",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
                             )
-
                             else -> CustomPortalItemView()
                         }
                     }, onClick = {})
@@ -123,7 +173,10 @@ private fun PortalList(
     onClick: ((Click) -> Unit)? = null,
     onLongClick: ((Click.OnLongClick) -> Unit)? = null,
 ) {
-    LazyRow() {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(8.dp)
+    ) {
         items(items, key = { it.id }) {
             renderView(it)
         }
--- app/src/main/java/com/example/portals/ui/components/PortalItemView.kt
+++ app/src/main/java/com/example/portals/ui/components/PortalItemView.kt
@@ -1,7 +1,15 @@
 package com.example.portals.ui.components
 
+import android.view.MotionEvent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,14 +17,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -34,29 +48,58 @@ class Ring(
     val color: Color = Color.LightGray
 )
 
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun DefaultItemView(
     modifier: Modifier = Modifier,
     ring: Ring = Ring(),
-    image: Image
+    image: Image,
+    onClick: () -> Unit,
+    onLongClick: (() -> Unit)? = null
 ) {
     val showSkeleton = remember { mutableStateOf(true) }
     val showFallback = remember { mutableStateOf(false) }
 
-    val ringPadding = if (ring.width > 0.dp) 10.dp else 0.dp
+    val selected = remember { mutableStateOf(false) }
+
+    val scale = animateFloatAsState(
+        if (selected.value) 0.9f else 1f,
+        label = "",
+
+        animationSpec = tween(durationMillis = 300)
+    )
+
+    val ringPadding = ring.width * 2
 
     Box(
         modifier = modifier
+            .scale(scale.value)
             .border(
                 width = ring.width,
                 shape = CircleShape,
                 color = ring.color
             )
             .padding(ringPadding)
+            .pointerInput(Unit){
+                detectTapGestures(
+                    onPress = {
+                        selected.value = true
+                        tryAwaitRelease()
+                        selected.value = false
+                    },
+                    onTap = {
+                        onClick()
+                    },
+                    onLongPress = {
+                        onLongClick?.invoke()
+                    }
+                )
+            }
+
     ) {
         Box(
             modifier = modifier
-                .size(100.dp)
+                .size(76.dp)
                 .clip(CircleShape)
                 .background(Color.LightGray),
             contentAlignment = Alignment.Center
@@ -68,6 +111,7 @@ fun DefaultItemView(
                 onSuccess = {
                     showSkeleton.value = false
                 },
+                contentScale = ContentScale.Fit,
                 onError = {
                     showSkeleton.value = false
                     showFallback.value = true
@@ -97,6 +141,7 @@ fun DefaultItemViewPreview() {
             source = "https://www.picsum.photos/100/100",
             contentDescription = "text"
         ),
-        ring = Ring(width = 6.dp)
+        ring = Ring(width = 3.dp),
+        onClick = {},
     )
 }
